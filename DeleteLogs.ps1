6382e75affe6baaca0a1e46fd43e908256d3e95c
Maulik Kanani <Maulik@hq.pronexus.com>
2020-11-08 17:40:30 -0500

    PS cript to deletelogs files

---
+++ DeleteLogs.ps1
@@ -0,0 +1,8 @@
+﻿# Delete all Log Files older than 60 day(s)
+
+$Path ="C:\Vocantas\EmployeePortal\Logs\VQA-QA4"
+$Daysback = "-1"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item -Recurse -Force -Confirm:$false
